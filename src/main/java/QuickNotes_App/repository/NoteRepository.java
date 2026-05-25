d826d8cc7bc4a33585704b5b268fbb99d633046e
Sadi Narendhra <narendhrasadi@gmail.com>
2026-05-25 08:28:23 +0530

    Update NoteRepository.java

--- src/main/java/QuickNotes_App/repository/NoteRepository.java
+++ src/main/java/QuickNotes_App/repository/NoteRepository.java
@@ -10,4 +10,13 @@ import java.util.List;
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
     List<Note> findByUser(User user);
+
+    // SEARCH NOTES
+    List<Note> findByUserAndTitleContainingIgnoreCaseOrUserAndContentContainingIgnoreCase(
+            User user1,
+            String title,
+
+            User user2,
+            String content
+    );
 }
